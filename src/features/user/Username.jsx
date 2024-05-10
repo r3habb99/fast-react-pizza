c7f5e2864d019986de9df923824d9df849a900f6
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-10 11:18:52 +0530

    Added basic tailwind css in jsx and added media queries

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm text-semibold">Rishabh</div>;
+  return <div className="text-sm text-semibold hidden md:block">Rishabh</div>;
 }
 
 export default Username;
